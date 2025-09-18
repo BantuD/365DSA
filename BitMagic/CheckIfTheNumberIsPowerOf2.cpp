3b64806dc6b81140f31da11bcb6cf075c1e2eb66
Rajul Dubey <46771371+BantuD@users.noreply.github.com>
2025-09-18 11:33:40 +0530

    Update and rename CheckIfTheNumberIsPowerOf2 to CheckIfTheNumberIsPowerOf2.cpp

---
+++ BitMagic/CheckIfTheNumberIsPowerOf2.cpp
@@ -0,0 +1,5 @@
+/*
+Logic: 
+-> If the number is not the power of 2 then if we subtract 1 from the number and do AND operation with the orginal number it won't give 0 otherwise it will give you zero
+Example: 
+*/
