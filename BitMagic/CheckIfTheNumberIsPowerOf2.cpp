67fc553b3b288c8f93e525c2bc197235e961bd1b
Rajul Dubey <46771371+BantuD@users.noreply.github.com>
2025-09-18 11:36:42 +0530

    Update CheckIfTheNumberIsPowerOf2.cpp

--- BitMagic/CheckIfTheNumberIsPowerOf2.cpp
+++ BitMagic/CheckIfTheNumberIsPowerOf2.cpp
@@ -2,4 +2,10 @@
 Logic: 
 -> If the number is not the power of 2 then if we subtract 1 from the number and do AND operation with the orginal number it won't give 0 otherwise it will give you zero
 Example: 
+x = 7(0111); y = x-1 => 6(0110) 
+result x&y
+if(result > 0)
+{
+print(number is not the power of 2)
+}
 */
